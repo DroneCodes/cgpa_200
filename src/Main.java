a5600f4dc61d043938e79c0e9f2f8120d70a0022
DroneCodes <oluwafisayomiogunyemi@gmail.com>
2024-03-20 12:55:35 -0400

    Added new comments for easy understanding

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         List<Student> students = new ArrayList<>();
 
-        // Read file and populate students list
+        // This block of code reads student data from a text file.
+        // Each line in the file represents a student's course result.
+        // The line is split into parts, each part representing a different piece of data.
+        // The student ID, course code, credit hours, and grade are extracted from the line.
+        // A new Course object is created with the course code and credit hours.
+        // A new Result object is created with the Course object and the grade.
+        // The findOrCreateStudent method is called with the students list and the student ID.
+        // This method returns a Student object, either an existing one with the same ID, or a new one if no such student exists.
+        // The Result object is added to the student's list of results.
+        // If an IOException occurs while reading the file, it is caught and a RuntimeException is thrown.
         try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -37,13 +46,18 @@ public class Main {
             throw new RuntimeException(e);
         }
 
-        // Calculate CGPA and write to file
+        // This block of code calculates the CGPA for each student and writes the results to a text file.
+        // A PrintWriter is created to write to the file.
+        // The calculateCGPA method of the cgpa class is called for each student.
+        // The student's ID and CGPA are written to the file, separated by a comma.
+        // If an IOException occurs while writing to the file, it is caught and a RuntimeException is thrown.
         try (PrintWriter writer = new PrintWriter(new FileWriter("output.txt"))) {
             for (Student student : students) {
                 double cgpa = classes.cgpa.calculateCGPA(student);
                 writer.println(student.id + "," + cgpa);
             }
         }
+
     }
 
     /**
